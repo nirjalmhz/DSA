6cd79c80a22a9dfbfca5e4a0409a46702858f3a6
nirjalmhz <156629886+nirjalmhz@users.noreply.github.com>
2026-05-29 09:52:49 +0545

    Add files via upload
    dsa stack program

---
+++ STACK.C
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int stack[SIZE];
+int top = -1;
+
+// Push operation
+void push(int value) {
+    if(top == SIZE - 1) {
+        printf("Stack Overflow\n");
+    }
+    else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed into stack\n", value);
+    }
+}
+
+// Pop operation
+void pop() {
+    if(top == -1) {
+        printf("Stack Underflow\n");
+    }
+    else {
+        printf("%d popped from stack\n", stack[top]);
+        top--;
+    }
+}
+
+// Top operation
+void peek() {
+    if(top == -1) {
+        printf("Stack is empty\n");
+    }
+    else {
+        printf("Top element is %d\n", stack[top]);
+    }
+}
+
+int main() {
+
+    push(10);
+    push(20);
+    push(30);
+
+    peek();
+
+    pop();
+
+    peek();
+
+    return 0;
+}
